6ab0001124706bf22452e807c73c2133032ba6a8
Rhault <raullloredagomez@gmail.com>
2022-11-15 21:27:23 -0300

    struture initial project eshop react

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+ 
+const App = () => {
+    return(
+        <h1>Vamos començar</h1>
+    );
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import react from "react"
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<App/>, document.getElementById("app"));
