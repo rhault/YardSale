dc1ef366bab067a7a7ccdede673a56b76499d6d9
Rhault <raullloredagomez@gmail.com>
2023-03-23 22:02:35 -0300

    fusao do menu mobile

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const mobile_menu_button = document.querySelector('.mobile-menu-button');
+const mobile_menu = document.querySelector('.mobile-menu');
 
-navbar_email.addEventListener('click', toggleDesktopMenu);
+navbar_email.addEventListener('click', () => toggleDesktopMenu(desktop_menu));
+mobile_menu_button.addEventListener('click', () =>  toggleDesktopMenu(mobile_menu));
 
-function toggleDesktopMenu() {
-    desktop_menu.classList.toggle('hidden')
+function toggleDesktopMenu(item) {
+    console.log('ok')
+    item.classList.toggle('hidden');
 }
